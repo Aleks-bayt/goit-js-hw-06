feecea56820065feeb08f0ac2223f85481c56621
Aleks-bayt <130595930+Aleks-bayt@users.noreply.github.com>
2024-03-04 16:17:12 +0200

    Update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -12,6 +12,9 @@ this.#items.push(newItem);
   }
 removeItem(itemToRemove) {
 const indexItems = this.#items.indexOf(itemToRemove);
+if (indexItems === -1) {
+  return;
+}
 this.#items.splice(indexItems,1);
 }
 
